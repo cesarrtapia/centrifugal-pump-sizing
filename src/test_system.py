2cfee0c95680b0a5c269d64b1b867fd587a0567a
cesarrtapia <157637297+cesarrtapia@users.noreply.github.com>
2026-04-09 19:53:35 -0600

    Update test_system.py

--- src/test_system.py
+++ src/test_system.py
@@ -1,1 +1,27 @@
+from calculations import calculate_system
+
+data = {
+    "fluid": {
+        "density": 998,
+        "viscosity": 0.001002
+    },
+    "flow_m3s": 0.0139,
+    "pipe": {
+        "diameter": 0.0762,
+        "length": 100,
+        "roughness": 0.000045
+    },
+    "system": {
+        "static_head": 10,
+        "K_total": 3.8
+    },
+    "pump": {
+        "efficiency": 0.7
+    }
+}
+
+results = calculate_system(data)
+
+for k, v in results.items():
+    print(f"{k}: {v}")
 
